29d4cce2a44efd9fa611361a7128f38b54998b1f
echoes1205 <leonardo.ochoaravelo@gmail.com>
2023-06-19 22:43:05 -0600

    fix method to login and change view for login

--- routes/login.js
+++ routes/login.js
@@ -3,13 +3,16 @@ var router = express.Router();
 var db = require("../conexion/conexion");
 
 
-// Configurar vista
-
-// Ruta para renderizar la página de registro
-router.get('/', function(req, res, next) {
-    db.query("SELECT * FROM usuarios", function (err, resultado) {
-        console.log(resultado);
-        res.render('login', { title: 'Iniciar sesion', user: resultado });
+router.post('/', (req, res, next) => {
+    const { user, password } = req.body;
+    const query = 'SELECT * FROM usuarios WHERE user = ? AND password = ?';
+    db.query(query, [user, password], (error, resultado) => {
+        if (error) throw error;
+        if (resultado.length > 0) {
+            res.send('Inicio de sesion exitoso');
+        } else {
+            res.send('Usuario o contraseña incorrectos');
+        }
     });
 });
 
